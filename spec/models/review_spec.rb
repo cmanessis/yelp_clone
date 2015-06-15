24fb0d49088a0fc39bbb3e2a9fa3d48b0d2e3df8
cmanessis <cmpetrowii@gmail.com>
2015-06-15 19:18:17 +0100

    unit test with shoulda

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,3 +3,7 @@ require 'rails_helper'
 RSpec.describe Review, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+describe Review do
+  it { should belong_to(:restaurant) }
+end
