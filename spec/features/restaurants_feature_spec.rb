314d85bc5ff68728b7506d60a90bc234a9345b1d
cmanessis <cmpetrowii@gmail.com>
2015-06-15 11:15:20 +0100

    second test passing

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+ require 'rails_helper'
 
 feature 'restaurants' do
   context 'no restaurants have been added' do
@@ -6,6 +6,18 @@ feature 'restaurants' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
       expect(page).to have_link 'Add a restaurant'
+    end
+  end
+
+  context 'restaurants have been added' do
+    before do
+      Restaurant.create(name: 'KFC')
+    end
+
+    scenario 'display restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content('KFC')
+      expect(page).not_to have_content('No restaurants yet')
     end
   end
 end
